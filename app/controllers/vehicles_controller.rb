035e1570ef43e8c3468d7c355d9b829513dfb496
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2021-03-04 08:10:03 -0600

    vehicles_controller

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -1,2 +1,23 @@
-class VehiclesController < ApplicationController
+class  VehiclesController < ApplicationController
+    before_action :authenticate_user!
+    def index
+        @vehicles = Vehicle.all
+    end
+
+    def new
+        @vehicle = Vehicle.new
+    end
+
+    def create
+        @vehicle = Vehicle.new(vehicle_params)
+        if @vehicle.save
+            redirect_to vehicles_path(@vehicle)
+        else
+            render :new
+        end
+    end
+
+    def vehicle_params
+        params.require(:vehicle).permit(:name)
+    end
 end
