d126ecf8dfe7eba1c97dc9aee3042f594fc779f8
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2021-03-04 08:17:29 -0600

    vehicle.rb

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,2 +1,9 @@
 class Vehicle < ApplicationRecord
+
+    # Has Many / Belongs To
+    has_many :cars
+    has_many :users, through: :cars
+
+    # Validations
+    validates :name, :presence => true
 end
